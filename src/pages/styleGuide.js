b182555bec54ee617249880527939b2d608c30be
Mircea Ungureanu <mircea.ungureanu@codecomputerlove.com>
2020-05-17 15:45:40 +0100

    :sparkles: feat: add styleguide page

---
+++ src/pages/styleGuide.js
@@ -0,0 +1,15 @@
+import  React from 'react'
+import {Link} from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const StyleGuide = () => (
+  <Layout>
+    <SEO title='Style guide' />
+    <h1>Style guide</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default StyleGuide
